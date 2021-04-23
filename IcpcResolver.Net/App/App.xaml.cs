8c443390c2e459fe1e70016f9ae40d39a8b5b216
QingQiz <sofeeys@outlook.com>
2021-04-24 03:19:22 +0800

    Initial commit 2

---
+++ IcpcResolver.Net/App/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace IcpcResolver.Net.App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ IcpcResolver.Net/UserControl/Team.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace IcpcResolver.Net.UserControl
+{
+    public partial class Team : System.Windows.Controls.UserControl
+    {
+        public Team()
+        {
+            InitializeComponent();
+        }
+
+        public int TeamRank
+        {
+            get => (int) GetValue(TeamRankProperty);
+            set => SetValue(TeamRankProperty, value);
+        }
+
+        public static readonly DependencyProperty TeamRankProperty =
+            DependencyProperty.Register("TeamRank", typeof(int), typeof(Team));
+
+        public string TeamName
+        {
+            get => (string) GetValue(TeamNameProperty);
+            set => SetValue(TeamNameProperty, value);
+        }
+
+        public static readonly DependencyProperty TeamNameProperty =
+            DependencyProperty.Register("TeamName", typeof(string), typeof(Team));
+
+    }
+}
