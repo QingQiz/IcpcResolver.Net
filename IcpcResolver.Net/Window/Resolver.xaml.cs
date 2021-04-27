f01c4159e9bae2481dd2d648f905e4930cdc0213
QingQiz <sofeeys@outlook.com>
2021-04-28 02:18:00 +0800

    feat: Resolver window background color

--- IcpcResolver.Net/Window/Resolver.xaml.cs
+++ IcpcResolver.Net/Window/Resolver.xaml.cs
@@ -1,54 +1,98 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 using IcpcResolver.Net.UserControl;
 
 namespace IcpcResolver.Net.Window
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Resolver.xaml
     /// </summary>
     public partial class Resolver : System.Windows.Window
     {
         public Resolver()
         {
             InitializeComponent();
+            InitGrid();
 
             var values = Enum.GetValues(typeof(ProblemStatus));
             var random = new Random();
 
-            ProblemDto GetProblem(int n) => new()
+            ProblemDto GetProblem(int n)
             {
-                Label = new string(new[] {(char) ('A' + n)}),
-                Status = (ProblemStatus) (values.GetValue(random.Next(values.Length)) ?? ProblemStatus.NotTried),
-                Time = random.Next(1, 300), Try = random.Next(1, 5)
-            };
+                var status = (ProblemStatus) (values.GetValue(random.Next(values.Length)) ?? ProblemStatus.NotTried);
+
+                return new ProblemDto
+                {
+                    Label = new string(new[] {(char) ('A' + n)}),
+                    Status = status,
+                    Time = status == ProblemStatus.NotTried ? 0 : random.Next(1, 300),
+                    Try = status == ProblemStatus.NotTried ? 0 : random.Next(1, 5)
+                };
+            }
 
             const int problemN = 16;
 
-            var teams = Enumerable.Range(0, MaxTeamNumberToDisplay)
-                .Select(n => new TeamDto
+            var teamDtos = Enumerable
+                .Range(0, MaxTeamNumberToDisplay)
+                .Select(n =>
                 {
-                    Rank = n,
-                    Name = "Team" + n,
-                    Problems = Enumerable.Range(0, problemN).Select((Func<int, ProblemDto>) GetProblem)
-                });
+                    // NOTE there must be `ToList`
+                    var problems = Enumerable
+                        .Range(0, problemN).Select((Func<int, ProblemDto>) GetProblem)
+                        .ToList();
+                    return new TeamDto
+                    {
+                        Rank = 0,
+                        Name = "Team" + n,
+                        Problems = problems
+                    };
+                })
+                .OrderByDescending(t => t.AcceptedCount)
+                .ThenBy(t => t.ScoreAll)
+                .ToList();
 
-            var cnt = 0;
-            foreach (var t in teams)
+            for (var i = 0; i < Math.Min(MaxTeamNumberToDisplay, teamDtos.Count); i++)
             {
-                var team = new Team(t);
-
-                Teams.RowDefinitions.Add(new RowDefinition());
+                var team = new Team(teamDtos[i]);
+            
                 Teams.Children.Add(team);
-                
-                Grid.SetRow(team, cnt++);
+                _teams.Add(team);
+            
+                Grid.SetRow(team, i);
                 Grid.SetColumn(team, 0);
             }
         }
 
-        public const int MaxTeamNumberToDisplay = 12;
+        private const int MaxTeamNumberToDisplay = 13;
+
+        private List<Team> _teams = new();
+
+        /// <summary>
+        /// init grid background color to (gray, black, gray, black...)
+        /// </summary>
+        private void InitGrid()
+        {
+            for (var i = 0; i <= MaxTeamNumberToDisplay; i++)
+            {
+                var border = new Border
+                {
+                    Background = (i & 1) == 0
+                        ? new SolidColorBrush(Color.FromRgb(0x3c, 0x3c, 0x3c))
+                        : new SolidColorBrush(Color.FromRgb(0, 0, 0)),
+                    Height = 84
+                };
+                
+                Teams.RowDefinitions.Add(new RowDefinition());
+                Teams.Children.Add(border);
+                
+                Grid.SetRow(border, i);
+                Grid.SetColumn(border, 0);
+            }
+        }
 
         protected override void OnKeyDown(KeyEventArgs e)
         {
