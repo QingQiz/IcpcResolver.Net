ee2538d45ef975cc6150e6bfc814a57c51ac7bad
QingQiz <sofeeys@outlook.com>
2021-05-02 21:21:13 +0800

    feat: init config window

---
+++ IcpcResolver.Net/Window/Loader.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace IcpcResolver.Net.Window
+{
+    /// <summary>
+    /// Interaction logic for Loader.xaml
+    /// </summary>
+    public partial class Loader : System.Windows.Window
+    {
+        public Loader()
+        {
+            InitializeComponent();
+        }
+    }
+}
